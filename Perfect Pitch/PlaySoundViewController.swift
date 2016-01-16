80cf60459f2a35c086331cef1b8bdc416c7bcccd
yusong-shen <yusongtju@gmail.com>
2016-01-15 22:29:41 -0600

    Add a play sound controller.

---
+++ Perfect Pitch/PlaySoundViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PlaySoundViewController.swift
+//  Perfect Pitch
+//
+//  Created by 沈煜松 on 1/15/16.
+//  Copyright (c) 2016 Yusong Shen. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundViewController: UIViewController {
+   
+}

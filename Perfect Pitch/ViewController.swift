ce0f2739f8913fd0d5b100512c578e9a37bd3344
yusong-shen <yusongtju@gmail.com>
2016-01-15 22:01:44 -0600

    Add the viewWillAppear function.

--- Perfect Pitch/ViewController.swift
+++ Perfect Pitch/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var stopButton: UIButton!
     
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,8 +25,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        RecordingProcess.hidden = true
+        recordButton.enabled = true
+    }
 
     @IBAction func recordAudio(sender: UIButton) {
+        recordButton.enabled = false
         RecordingProcess.hidden = false
         stopButton.hidden = false
         println("recording")

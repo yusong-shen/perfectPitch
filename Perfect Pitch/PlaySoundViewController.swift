43f5bba7f9bb68b4f1c92101eae766990f60cdae
yusong-shen <yusongtju@gmail.com>
2016-01-16 09:48:10 -0600

    Add a stop button in second scene.

--- Perfect Pitch/PlaySoundViewController.swift
+++ Perfect Pitch/PlaySoundViewController.swift
@@ -16,7 +16,6 @@ class PlaySoundViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // file is empty ??
         if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
             var filePathUrl = NSURL.fileURLWithPath(filePath)
             audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error:nil)
@@ -36,8 +35,16 @@ class PlaySoundViewController: UIViewController {
     @IBAction func playLowPitch(sender: UIButton) {
         
     }
+    
     @IBAction func slowPlay(sender: UIButton) {
+        audioPlayer.stop()
         audioPlayer.rate = 0.5
+        audioPlayer.currentTime = 0
         audioPlayer.play()
     }
+    
+    @IBAction func stopButton(sender: UIButton) {
+        audioPlayer.stop()
+    }
+    
 }

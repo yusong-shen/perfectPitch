61c2590f01d3f05d3815d48a3acdceac916fb741
yusong-shen <yusongtju@gmail.com>
2016-01-16 09:24:58 -0600

    Use rate in audioPlayer to change the speech speed.

--- Perfect Pitch/PlaySoundViewController.swift
+++ Perfect Pitch/PlaySoundViewController.swift
@@ -20,6 +20,7 @@ class PlaySoundViewController: UIViewController {
         if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
             var filePathUrl = NSURL.fileURLWithPath(filePath)
             audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error:nil)
+            audioPlayer.enableRate = true
             
         }else{
             println("filepath is empty")
@@ -36,6 +37,7 @@ class PlaySoundViewController: UIViewController {
         
     }
     @IBAction func slowPlay(sender: UIButton) {
+        audioPlayer.rate = 0.5
         audioPlayer.play()
     }
 }
